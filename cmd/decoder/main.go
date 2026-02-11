b2aa430543b9570e03e8a49d0ce1d675f94ff180
Shubh <pingaleshubham600@gmail.com>
2026-02-11 08:30:22 +0530

    load transaction hex from file instead of hardcoded value

--- cmd/decoder/main.go
+++ cmd/decoder/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/shubhtrek/btc_tx_decoder/internal/decoder"
 )
@@ -12,7 +14,22 @@ func main() {
 	fmt.Println("Bitcoin Transaction Decoder")
 
 	// REAL Bitcoin transaction (testnet example)
-	hexTx := "02000000000101b1c2d3e4f5a6978899aabbccddeeff00112233445566778899aabbccddeeff0000000000ffffffff02e8030000000000001600144f3c1a2b3d4e5f60718293a4b5c6d7e8f901234567880000000000000000160014abcdefabcdefabcdefabcdefabcdefabcdefabcd02473044022011223344556677889900aabbccddeeff00112233445566778899aabbccddeeff022011223344556677889900aabbccddeeff00112233445566778899aabbccddeeff012102abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef"
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("  go run ./cmd/decoder <tx_hex_file>")
+		return
+	}
+
+	filePath := os.Args[1]
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("File read error:", err)
+		return
+	}
+
+	hexTx := strings.TrimSpace(string(data))
 
 	raw, err := hex.DecodeString(hexTx)
 	if err != nil {
